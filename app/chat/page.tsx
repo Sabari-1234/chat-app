d6f9d0f679a7601caf7a94981f5507f6bc992db8
Sabari-1234 <sabarinathan5802@gmail.com>
2024-07-23 16:22:28 +0530

    initial setup

---
+++ app/chat/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Enter message..."/>
+        <button>sent</button>
+      </div>
+    </div>
+  )
+}
+
+export default page
